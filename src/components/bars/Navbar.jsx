c284d6cc9c13db458a5f7a5fec40f14961001a73
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-08-18 17:39:59 -0500

    Scaffold Navbar & check links

--- src/components/bars/Navbar.jsx
+++ src/components/bars/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
+import { Link } from 'gatsby';
+
+import { Box, 
+         Container, 
+         Flex,
+         Text,
+         UnorderedList,
+         ListItem
+       } from '@chakra-ui/react';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Box>
+      <Container>
+        <Flex>
+          <Text>Logo</Text>
+          <UnorderedList>
+            <ListItem>
+              <Link to="/">App Overview</Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/download">Downlaod Guide</Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/our-team/sponsors">Project Sponsors</Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/our-team/developers">Dev Team</Link>
+            </ListItem>
+          </UnorderedList>
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 
