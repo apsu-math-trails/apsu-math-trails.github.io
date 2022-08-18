7f9c69a065a6808869c3580d6d7477f1d2527e47
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-08-18 17:40:39 -0500

    Create SocialButton component

--- src/components/buttons/SocialButton.jsx
+++ src/components/buttons/SocialButton.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
-const SocialButton = () => {
+import { Button, 
+         VisuallyHidden,
+         useColorModeValue,
+       } from '@chakra-ui/react';
+
+
+const SocialButton = ({ children, label, href }) => {
   return (
-    <div>SocialButton</div>
+    <Button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </Button>
   );
 };
 
