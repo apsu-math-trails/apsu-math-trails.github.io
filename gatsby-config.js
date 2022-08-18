29ae7655ff6b871c421171d546b8bd2d9072d1fb
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-08-18 10:31:11 -0500

    Install project packages & create file structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,49 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetaData: {
+    title: 'Apsu Math Trails - Official Site',
+    description: `Offical website for APSU College of STEM's Math Trails mobile app`,
+    siteUrl: `apsu-math-trails.github.io`,
+    image: `/gatsby-icon.png`,
+  },
+  plugins: [
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-image',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/assets/images`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'illustrations',
+        path: `${__dirname}/src/assets/illustrations`
+      }
+    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: 'logos',
+    //     path: `${__dirname}/src/assets/logos`
+    //   }
+    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+  ],
 }
