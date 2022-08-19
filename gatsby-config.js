6fbb9ef8da37ecc30036bb9f733995ffa56c01fe
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-08-18 22:42:24 -0500

    Include chakra plugin in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,28 @@ module.exports = {
     title: 'Apsu Math Trails - Official Site',
     description: `Offical website for APSU College of STEM's Math Trails mobile app`,
     image: `/gatsby-icon.png`,
+    organization: {
+      official: {
+        socialLinks: {
+          site: 'https://www.apsu.edu/',
+          twitter: 'https://twitter.com/austinpeay?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
+          facebook: 'https://www.facebook.com/austinpeay/',
+          instagram: 'https://www.instagram.com/austinpeay/?hl=en',
+        }
+      },
+      costem: {
+        socialLinks: {
+          site: 'https://www.apsu.edu/costem',
+          twitter: 'https://twitter.com/apsucostem',
+          facebook: 'https://www.facebook.com/APSUCoSTEM/',
+          instagram: 'https://www.instagram.com/apsucostem/?hl=en',
+          github: 'https://github.com/apsu-math-trails',
+        }
+      }
+    },
   },
   plugins: [
+    '@chakra-ui/gatsby-plugin',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-image',
