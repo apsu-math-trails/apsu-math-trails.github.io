ee456cfcfa75b7e819827e4365d5101c16ecf9ff
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-08-19 09:48:12 -0500

    Install gatsby-plugin-manifest & add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'Apsu Math Trails - Official Site',
+    title: 'Journey Today | Math Trails - APSU',
     description: `Offical website for APSU College of STEM's Math Trails mobile app`,
     image: `/gatsby-icon.png`,
     organization: {
@@ -55,6 +55,22 @@ module.exports = {
     //     path: `${__dirname}/src/assets/logos`
     //   }
     // },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `Math Trails`,
+        short_name: `Math Trails`,
+        description: `Mixing exploration and mathematics, math trails provides an engaging way to learn various levels of mathematics. Embark on your journey today.`,
+        lang: `en`,
+        icon: `src/assets/icons/android-chrome-512x512.png`,
+        start_url: `/`,
+        // background_color: `#663399`,
+        // theme_color: `#fff`,
+        // localize: [
+        //   // for locationization options, as {} in the array
+        // ],
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
