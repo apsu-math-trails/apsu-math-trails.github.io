11f5e84fa150156791bf941f8941080e172f64a8
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-09-10 22:36:59 -0500

    Create team-biographies.jsx

---
+++ src/data/team-biographies.jsx
@@ -0,0 +1,150 @@
+let teamBiographies = [
+  {
+    id: 1,
+    isProfessor: true,
+    role: 'Product Designer',
+    name: 'Jennifer Yantz',
+    bioDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus doloremque natus, sit vel quidem consequatur rem sunt unde, quia quae, saepe fuga cum sed.`,
+    avatar: {
+      isDefault: false,
+      avatarSources: {
+        defaultSrc: '',
+        src: '',
+      },
+    },
+    contactInformation: {
+      email: '123@gmail.com',
+      linkedin: null,
+      phone: null,
+    },
+    socialLinks: {
+      twitter: null,
+      instagram: null,
+      github: null,
+    }
+  },
+  {
+    id: 2,
+    isProfessor: true,
+    role: 'Product Designer',
+    name: 'Marylou Dalton',
+    bioDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus doloremque natus, sit vel quidem consequatur rem sunt unde, quia quae, saepe fuga cum sed.`,
+    avatar: {
+      isDefault: false,
+      avatarSources: {
+        defaultSrc: '',
+        src: '',
+      },
+    },
+    contactInformation: {
+      email: '456@gmail.com',
+      linkedin: null,
+      phone: null,
+    },
+    socialLinks: {
+      twitter: null,
+      instagram: null,
+      github: null,
+    }
+  },
+  {
+    id: 3,
+    isProfessor: true,
+    role: 'Developer',
+    name: 'James Church',
+    bioDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus doloremque natus, sit vel quidem consequatur rem sunt unde, quia quae, saepe fuga cum sed.`,
+    avatar: {
+      isDefault: true,
+      avatarSources: {
+        defaultSrc: '',
+        src: '',
+      },
+    },
+    contactInformation: {
+      email: '789@gmail.com',
+      linkedin: null,
+      phone: null,
+    },
+    socialLinks: {
+      twitter: null,
+      instagram: null,
+      github: null,
+    }
+  },
+  {
+    id: 4,
+    isProfessor: true,
+    role: 'Developer',
+    name: 'Kidane Millen',
+    bioDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus doloremque natus, sit vel quidem consequatur rem sunt unde, quia quae, saepe fuga cum sed.`,
+    avatar: {
+      isDefault: true,
+      avatarSources: {
+        defaultSrc: '',
+        src: '',
+      },
+    },
+    contactInformation: {
+      email: '123@gmail.com',
+      linkedin: null,
+      phone: null,
+    },
+    socialLinks: {
+      twitter: null,
+      instagram: null,
+      github: null,
+    }
+  },
+  {
+    id: 5,
+    isProfessor: true,
+    role: 'Developer',
+    name: 'Nicholas Fox',
+    bioDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus doloremque natus, sit vel quidem consequatur rem sunt unde, quia quae, saepe fuga cum sed.`,
+    avatar: {
+      isDefault: true,
+      avatarSources: {
+        defaultSrc: '',
+        src: '',
+      },
+    },
+    contactInformation: {
+      email: '123@gmail.com',
+      linkedin: null,
+      phone: null,
+    },
+    socialLinks: {
+      twitter: null,
+      instagram: null,
+      github: null,
+    }
+  },
+  {
+    id: 6,
+    isProfessor: true,
+    role: 'Developer',
+    name: 'Johnathan Dickson',
+    bioDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus doloremque natus, sit vel quidem consequatur rem sunt unde, quia quae, saepe fuga cum sed.`,
+    avatar: {
+      isDefault: true,
+      avatarSources: {
+        defaultSrc: '',
+        src: '',
+      },
+    },
+    contactInformation: {
+      email: '123@gmail.com',
+      linkedin: null,
+      phone: null,
+    },
+    socialLinks: {
+      twitter: null,
+      instagram: null,
+      github: null,
+    }
+  },
+];
+
+export function getTeamBiographies() {
+  return teamBiographies;
+};
